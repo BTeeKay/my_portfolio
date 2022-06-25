87179702262c2640b855e9eb4d9f77cda151e7bb
Brian Kerr <briantkerr91@gmail.com>
2022-06-25 18:35:14 +0100

    added lorem ipsum

--- src/components/HomeBox.js
+++ src/components/HomeBox.js
@@ -1,9 +1,29 @@
 import React from 'react';
+import './homebox.css'
 
 const HomeBox = () => {
 
     return (
-        <h1>This is home box</h1>
+        <div className="HomeBox">
+        <h2>About</h2>
+        <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. In sit amet lacus quis purus 
+        euismod commodo eget quis ex. Proin vel tellus eget neque euismod maximus. Etiam eu dapibus 
+        mauris, eget aliquet risus. Aenean sed nulla a lacus euismod congue. Nam lacinia risus sit amet 
+        fermentum semper. Duis quis diam orci. Duis magna nunc, pretium sit amet volutpat placerat, fermentum non metus. 
+        Duis ac mollis mauris, interdum tristique massa. Suspendisse lacinia justo quis tortor volutpat, eu vulputate odio 
+        pulvinar. Quisque in ipsum quam. Nunc rhoncus pulvinar risus, et tempor nunc varius eget. Nullam vehicula ipsum ut 
+        lectus lacinia varius. Cras aliquam pellentesque nisi, eget efficitur lacus ullamcorper nec. Vestibulum eget neque 
+        a urna bibendum convallis vitae a massa. In elementum lacinia gravida.
+        </p>
+        <p>
+        Curabitur auctor nulla at ante scelerisque, ac malesuada purus pulvinar. Duis fermentum ex ut felis condimentum 
+        maximus. In dignissim maximus tellus, ut bibendum lectus rutrum et. Morbi in erat vitae sem convallis egestas. 
+        Quisque tellus mi, iaculis ut lobortis eu, hendrerit sit amet neque. Morbi mollis condimentum urna, et accumsan 
+        libero pellentesque in. Aliquam erat volutpat. Quisque nec auctor quam. In sodales lectus vel arcu porta, vel 
+        ullamcorper quam faucibus. Ut tristique porta libero, nec iaculis ex efficitur congue. 
+        </p>
+        </div>
     )
 };
 
